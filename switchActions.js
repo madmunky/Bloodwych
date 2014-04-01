22fef82580a1c42f67c47d7fa7e0c4dc1402b1fd
madmunky <madmunkey@gmail.com>
2014-04-01 17:22:56 +0100

    Fixed Switches to only toggle D Byte

--- switchActions.js
+++ switchActions.js
@@ -32,36 +32,36 @@ function actionType(sw,p){
     
      switch (sw[0]){
          
-        case 2: {tw.Levels[p.level].Map[sw[2]][sw[3]] = "0000";}break;
+        case 2: {tw.Levels[p.level].Map[sw[2]][sw[3]] = tw.Levels[p.level].Map[sw[2]][sw[3]].replaceAt(3,"0");}break;
         case 4: {tw.Levels[p.level].Map[sw[2]][sw[3]] = toggleWall(tw.Levels[p.level].Map[sw[2]][sw[3]]);};break;
-        case 6: {};break;  //JORG PLEASE FIX THIS!!!
+        case 6: {tw.Levels[p.level].Map[sw[2]][sw[3]] = tw.Levels[p.level].Map[sw[2]][sw[3]].replaceAt(3,"0");}break;
         case 8: {};break;  //JORG PLEASE FIX THIS!!!
         case 10: {tw.Levels[p.level].Map[sw[2]][sw[3]] = togglePillar(tw.Levels[p.level].Map[sw[2]][sw[3]]);};break;
-        case 12: {tw.Levels[p.level].Map[sw[2]][sw[3]] = "0000";}break;
+        case 12: {tw.Levels[p.level].Map[sw[2]][sw[3]] = tw.Levels[p.level].Map[sw[2]][sw[3]].replaceAt(3,"0");}break;
         case 14: {};break;  //JORG PLEASE FIX THIS!!!
-         
+        default:{window.alert("Unhandled Switch Action: " + sw.toString());}
      }
     
 }
 
 function toggleWall(hex){
     
-    if (hex === "0001"){
-        return "0000";
+    if (hex.substring(3,4) === "1"){
+        return hex.replaceAt(3,"0");
     }
     else {
-        return "0001";
+        return hex.replaceAt(3,"1");
     }
 
 }
 
 function togglePillar(hex){
     
-    if (hex === "0103"){
-        return "0000";
+    if (hex.substring(3,4) === "3"){
+        return hex.replaceAt(3,"0");
     }
     else {
-        return "0103";
+        return hex.replaceAt(3,"3");
     }
 
 }
