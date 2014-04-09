d7fc0f6c9f3ab338d875785e7298c5d81d239d29
madmunky <madmunkey@gmail.com>
2014-04-09 11:49:26 +0100

    Added KeyCodes

--- input.js
+++ input.js
@@ -48,41 +48,68 @@ function doTouchStart(e) {
         
 }
 
+var KEYPAD_8 = 104,
+        KEYPAD_4 = 100,
+        KEYPAD_5 = 101,
+        KEYPAD_6 = 102,
+        KEYPAD_7 = 103,
+        KEYPAD_8 = 104,
+        KEYPAD_9 = 105,
+        KEY_END = 96,
+        KEYPAD_PLUS = 107,
+        KEY_L = 76,
+        KEY_T = 84,
+        KEY_SPACEBAR = 32,
+        KEY_G = 71,
+        KEY_W = 87,
+        KEY_S = 83,
+        KEY_A = 65,
+        KEY_D = 68,
+        KEY_Q = 81,
+        KEY_E = 69,
+        KEY_R = 82,
+        KEY_F = 70,
+        KEY_9 = 57,
+        KEY_0 = 48,
+        KEY_MINUS = 189,
+        KEY_PLUS = 187;
+
+
 function doKeyDown(e) {
 
     switch (e.keyCode)
     {
         
-        case 104: player[1].move(DIRECTION_NORTH);break; //8
-        case 101: player[1].move(DIRECTION_SOUTH);break; //5
-        case 100: player[1].move(DIRECTION_WEST);break; //4
-        case 102: player[1].move(DIRECTION_EAST);break; //6
-        case 103: player[1].rotateTo(player[1].Rotation - 1);break; //7
-        case 105: player[1].rotateTo(player[1].Rotation + 1);break; //9
-        case  96: player[1].action();break; //End Key
-        case 107: player[1].toggleFrontObject();break; //+ Key
-        case 76: // THE L KEY
+        case KEYPAD_8: player[1].move(DIRECTION_NORTH);break; //8
+        case KEYPAD_5: player[1].move(DIRECTION_SOUTH);break; //5
+        case KEYPAD_4: player[1].move(DIRECTION_WEST);break; //4
+        case KEYPAD_6: player[1].move(DIRECTION_EAST);break; //6
+        case KEYPAD_7: player[1].rotateTo(player[1].Rotation - 1);break; //7
+        case KEYPAD_9: player[1].rotateTo(player[1].Rotation + 1);break; //9
+        case KEY_END: player[1].action();break; //End Key
+        case KEYPAD_PLUS: player[1].toggleFrontObject();break; //+ Key
+        case KEY_L: // THE L KEY
             CurrentMap = (CurrentMap + 1) % Maps.length;
             tw = new Tower(Maps[CurrentMap]);
             player[0].X = 1; player[1].X = 2;
             player[0].Y = 1; player[1].Y = 1;
             player[0].setPlayerLevel(0); player[1].setPlayerLevel(0);
             break;
-        case 84: player[0].changeUpLevel();break;    // T KEY     
-        case 32: player[0].action();break;           // SpaceBar        
-        case 71: player[0].changeDownLevel(); break; // G KEY  
-        case 87: player[0].move(DIRECTION_NORTH);break;      // W KEY
-        case 83: player[0].move(DIRECTION_SOUTH);break;    // S KEY
-        case 65: player[0].move(DIRECTION_WEST);break;         // A KEY
-        case 68: player[0].move(DIRECTION_EAST);break;        // D KEY
-        case 81: player[0].rotateTo(player[0].Rotation - 1);break;    // Q KEY
-        case 69: player[0].rotateTo(player[0].Rotation + 1);break;    // E KEY
-        case 82: player[0].toggleFrontObject();break; //R Key
-        case 70: player[0].testMode();break; // F cheat
-        case 187: test++; break;
-        case 189: test--; break;
-        case 48: test2++; break;
-        case 57: test2--; break;
+        case KEY_T: player[0].changeUpLevel();break;    // T KEY     
+        case KEY_SPACEBAR: player[0].action();break;           // SpaceBar        
+        case KEY_G: player[0].changeDownLevel(); break; // G KEY  
+        case KEY_W: player[0].move(DIRECTION_NORTH);break;      // W KEY
+        case KEY_S: player[0].move(DIRECTION_SOUTH);break;    // S KEY
+        case KEY_A: player[0].move(DIRECTION_WEST);break;         // A KEY
+        case KEY_D: player[0].move(DIRECTION_EAST);break;        // D KEY
+        case KEY_Q: player[0].rotateTo(player[0].Rotation - 1);break;    // Q KEY
+        case KEY_E: player[0].rotateTo(player[0].Rotation + 1);break;    // E KEY
+        case KEY_R: player[0].toggleFrontObject();break; //R Key
+        case KEY_F: player[0].testMode();break; // F cheat
+        case KEY_PLUS: test++; break;
+        case KEY_MINUS: test--; break;
+        case KEY_0: test2++; break;
+        case KEY_9: test2--; break;
         default: break;
     }
 
