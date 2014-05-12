bff794b8c0040830306635e5a94ac5dd318223e4
jnieberg <jnieberg@gmail.com>
2014-05-13 00:44:39 +0200

    TEST multiplayer demo

--- !trash/multiplay/main.js
+++ !trash/multiplay/main.js
@@ -1,31 +1,48 @@
 var http = require('http'),
-	fs = require('fs');
+    fs = require('fs');
+var clientCount = 0;
 
 var app = http.createServer(function(request, response) {
-	fs.readFile("client.html", 'utf-8', function(error, data) {
-		response.writeHead(200, {
-			'Content-Type': 'text/html'
-		});
-		response.write(data);
-		response.end();
-	});
+    fs.readFile("client.html", 'utf-8', function(error, data) {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        response.write(data);
+        response.end();
+    });
 }).listen(1337);
 
 var io = require('socket.io').listen(app);
 
 io.sockets.on('connection', function(socket) {
-	fs.readFile('./data.txt', 'utf8', function(err, data) {
-		if (err) {
-			return console.log(err);
-		}
-		io.sockets.emit("init_to_client", {
-			message: data
-		});
-	});
+    fs.readFile('./data.txt', 'utf8', function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        io.sockets.emit("init_to_client", {
+            message: data
+        });
+    });
 
-	socket.on('message_to_server', function(data) {
-		io.sockets.emit("message_to_client", {
-			message: data["message"]
-		});
-	});
+    socket.on('message_to_server', function(data) {
+        io.sockets.emit("message_to_client", {
+            message: data["message"]
+        });
+    });
+
+    clientCount++;
+    socket.emit('online_count_to_client', {
+        count: clientCount
+    });
+
+    socket.on('disconnect', function() {
+        clientCount--;
+        io.sockets.emit('online_count_to_client', {
+            count: clientCount
+        });
+    });
+
+    socket.on('close_to_server', function(data) {
+    	fs.writeFile('./data.txt', data["message"], 'UTF-8');
+    });
 });
