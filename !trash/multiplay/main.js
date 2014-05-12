59b92f83f6b06f4ba4233b9f0f00bc1c82d9351e
jnieberg <jnieberg@gmail.com>
2014-05-12 17:32:58 +0200

    TEST for nodejs

--- !trash/multiplay/main.js
+++ !trash/multiplay/main.js
@@ -1,18 +1,31 @@
 var http = require('http'),
-    fs = require('fs');
- 
-var app = http.createServer(function (request, response) {
-    fs.readFile("client.html", 'utf-8', function (error, data) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);
-        response.end();
-    });
+	fs = require('fs');
+
+var app = http.createServer(function(request, response) {
+	fs.readFile("client.html", 'utf-8', function(error, data) {
+		response.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		response.write(data);
+		response.end();
+	});
 }).listen(1337);
- 
+
 var io = require('socket.io').listen(app);
- 
+
 io.sockets.on('connection', function(socket) {
-    socket.on('message_to_server', function(data) {
-        io.sockets.emit("message_to_client",{ message: data["message"] });
-    });
+	fs.readFile('./data.txt', 'utf8', function(err, data) {
+		if (err) {
+			return console.log(err);
+		}
+		io.sockets.emit("init_to_client", {
+			message: data
+		});
+	});
+
+	socket.on('message_to_server', function(data) {
+		io.sockets.emit("message_to_client", {
+			message: data["message"]
+		});
+	});
 });
