812fdcb0069076acb5b2609ced205cef566323ba
Chris Richardson <madmunkey@gmail.com>
2014-05-19 12:00:21 +0100

    Added Touch ViewPort Moving

--- input.js
+++ input.js
@@ -149,6 +149,7 @@ function touchXY(e) {
         var y = e.targetTouches[0].pageY - canvas.offsetTop;
         var xy;
         processCanvasInput(x, y, xy);
+        viewportTouch(x , y, xy);
     }
 }
 
@@ -168,23 +169,30 @@ function processCanvasInput(x, y, xy) {
     }
 }
 
-function viewportTouch(){
+function viewportTouch(x,y,xy){
     
-    if (xy.x >= 0 && xy.x <= 2 && xy.y >= 0 && xy.y <= 2) {
-            if (xy.x === 0 && xy.y === 0) { //rotate left
-                player[p].rotateTo(player[p].d - 1);
-            } else if (xy.x === 2 && xy.y === 0) { //rotate right
-                player[p].rotateTo(player[p].d + 1);
-            } else if (xy.x === 1 && xy.y === 0) { //move forward
-                player[p].move(DIRECTION_NORTH);
-            } else if (xy.x === 0 && xy.y === 1) { //move left
-                player[p].move(DIRECTION_WEST);
-            } else if (xy.x === 2 && xy.y === 1) { //move right
-                player[p].move(DIRECTION_EAST);
-            } else if (xy.x === 1 && xy.y === 1) { //move backward
-                player[p].move(DIRECTION_SOUTH);
-            } else if (xy.y === 2) { //action
-                player[p].action();
+    xy = {
+            x: Math.floor((x - player[p].PortalX) / scale / 42.67),
+            y: Math.floor((y - player[p].PortalY) / scale / 38)
+    };
+
+    for (p = 0; p < 2; p++) {
+        if (xy.x >= 0 && xy.x <= 2 && xy.y >= 0 && xy.y <= 2) {
+                if (xy.x === 0 && xy.y === 0) { //rotate left
+                    player[p].rotateTo(player[p].d - 1);
+                } else if (xy.x === 2 && xy.y === 0) { //rotate right
+                    player[p].rotateTo(player[p].d + 1);
+                } else if (xy.x === 1 && xy.y === 0) { //move forward
+                    player[p].move(DIRECTION_NORTH);
+                } else if (xy.x === 0 && xy.y === 1) { //move left
+                    player[p].move(DIRECTION_WEST);
+                } else if (xy.x === 2 && xy.y === 1) { //move right
+                    player[p].move(DIRECTION_EAST);
+                } else if (xy.x === 1 && xy.y === 1) { //move backward
+                    player[p].move(DIRECTION_SOUTH);
+                } else if (xy.y === 2) { //action
+                    player[p].action();
+                }
             }
-        }
+    }
 }
