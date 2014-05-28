7d24f1d08db3dd633a20a9c27dd8cac550f097b0
Chris Richardson <madmunkey@gmail.com>
2014-05-28 17:07:40 +0100

    Colour DungeonItems Completed

--- testing.js
+++ testing.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 //var maleCharacterSpriteLocations = characterSpriteLocation();
-var testMon1 = 48,
+var testMon1 = 96,
         testDistance = 0,
         testDirection = 0,
         testPalette = 0;
