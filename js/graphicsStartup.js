3c01591dc934ea565d6754bba4c126ca7b466bbd
Jorg Nieberg <jnieberg@gmail.com>
2016-02-24 12:11:45 +0100

    removed UI_GFX_ prefix from vars.

--- js/graphicsStartup.js
+++ js/graphicsStartup.js
@@ -98,7 +98,7 @@ function drawQuickStartUI(pl) {
             var ch = champion[chN];
             var t = createShield(ch.id, ch.prof, ch.colour);
             if (players === 2 && currentPlayer === 1 && ch.id === championSelect[0].champID) {
-                ctx.drawImage(gfxUI['UI_GFX_SHIELD_BLUE'], (col * 40) * scale, ((row * 48) + 15) * scale, gfxUI['UI_GFX_SHIELD_BLUE'].width * scale, gfxUI['UI_GFX_SHIELD_BLUE'].height * scale);
+                ctx.drawImage(gfxUI['SHIELD_BLUE'], (col * 40) * scale, ((row * 48) + 15) * scale, gfxUI['SHIELD_BLUE'].width * scale, gfxUI['SHIELD_BLUE'].height * scale);
             } else {
                 if (championSelect[pl].champID !== -1) {
                     if (championSelect[pl].champID !== -1 && ch.id === championSelect[pl].champID) {
@@ -120,17 +120,17 @@ function drawQuickStartUI(pl) {
     var imageColour;
 
     if (pl === 0) {
-        imageColour = 'UI_GFX_CHARACTER_NAME_BLUE';
+        imageColour = 'CHARACTER_NAME_BLUE';
     } else {
-        imageColour = 'UI_GFX_CHARACTER_NAME_RED';
+        imageColour = 'CHARACTER_NAME_RED';
     }
     if (championSelect[pl].champID === -1) {
         var myY = 50,
             myX = 0;
 
-        ctx.drawImage(gfxUI[imageColour], 168 * scale, (myY + 75) * scale, gfxUI['UI_GFX_CHARACTER_NAME_BLUE'].width * scale, gfxUI['UI_GFX_CHARACTER_NAME_BLUE'].height * scale);
-        ctx.drawImage(gfxUI['UI_GFX_CHARACTER_BOX'], 168 * scale, (myY) * scale, gfxUI['UI_GFX_CHARACTER_BOX'].width * scale, gfxUI['UI_GFX_CHARACTER_BOX'].height * scale);
-        ctx.drawImage(gfxUI['UI_GFX_CHARACTER_SCROLL'], (myX + 226) * scale, (myY - 1) * scale, gfxUI['UI_GFX_CHARACTER_SCROLL'].width * scale, gfxUI['UI_GFX_CHARACTER_SCROLL'].height * scale);
+        ctx.drawImage(gfxUI[imageColour], 168 * scale, (myY + 75) * scale, gfxUI['CHARACTER_NAME_BLUE'].width * scale, gfxUI['CHARACTER_NAME_BLUE'].height * scale);
+        ctx.drawImage(gfxUI['CHARACTER_BOX'], 168 * scale, (myY) * scale, gfxUI['CHARACTER_BOX'].width * scale, gfxUI['CHARACTER_BOX'].height * scale);
+        ctx.drawImage(gfxUI['CHARACTER_SCROLL'], (myX + 226) * scale, (myY - 1) * scale, gfxUI['CHARACTER_SCROLL'].width * scale, gfxUI['CHARACTER_SCROLL'].height * scale);
 
     }
 
@@ -144,17 +144,17 @@ function showCharacterDetails(ch, pl, col, row) {
     var a = ch.prof;
     var b = ch.colour;
 
-    ctx.drawImage(gfxUI['UI_GFX_POCKET_' + a][b], 198 * scale, (myY + 57) * scale, gfxUI['UI_GFX_POCKET_EMPTY'].width * scale, gfxUI['UI_GFX_POCKET_EMPTY'].height * scale);
+    ctx.drawImage(gfxUI['POCKET_' + a][b], 198 * scale, (myY + 57) * scale, gfxUI['POCKET_EMPTY'].width * scale, gfxUI['POCKET_EMPTY'].height * scale);
     if (pl === 0) {
-        ctx.drawImage(gfxUI['UI_GFX_SHIELD_BLUE'], (col * 40) * scale, ((row * 48) + 15) * scale, gfxUI['UI_GFX_SHIELD_BLUE'].width * scale, gfxUI['UI_GFX_SHIELD_BLUE'].height * scale);
-        ctx.drawImage(gfxUI['UI_GFX_CHARACTER_NAME_BLUE'], 168 * scale, (myY + 75) * scale, gfxUI['UI_GFX_CHARACTER_NAME_BLUE'].width * scale, gfxUI['UI_GFX_CHARACTER_NAME_BLUE'].height * scale);
+        ctx.drawImage(gfxUI['SHIELD_BLUE'], (col * 40) * scale, ((row * 48) + 15) * scale, gfxUI['SHIELD_BLUE'].width * scale, gfxUI['SHIELD_BLUE'].height * scale);
+        ctx.drawImage(gfxUI['CHARACTER_NAME_BLUE'], 168 * scale, (myY + 75) * scale, gfxUI['CHARACTER_NAME_BLUE'].width * scale, gfxUI['CHARACTER_NAME_BLUE'].height * scale);
     } else {
-        ctx.drawImage(gfxUI['UI_GFX_SHIELD_RED'], (col * 40) * scale, ((row * 48) + 15) * scale, gfxUI['UI_GFX_SHIELD_BLUE'].width * scale, gfxUI['UI_GFX_SHIELD_BLUE'].height * scale);
-        ctx.drawImage(gfxUI['UI_GFX_CHARACTER_NAME_RED'], 168 * scale, (myY + 75) * scale, gfxUI['UI_GFX_CHARACTER_NAME_BLUE'].width * scale, gfxUI['UI_GFX_CHARACTER_NAME_BLUE'].height * scale);
+        ctx.drawImage(gfxUI['SHIELD_RED'], (col * 40) * scale, ((row * 48) + 15) * scale, gfxUI['SHIELD_BLUE'].width * scale, gfxUI['SHIELD_BLUE'].height * scale);
+        ctx.drawImage(gfxUI['CHARACTER_NAME_RED'], 168 * scale, (myY + 75) * scale, gfxUI['CHARACTER_NAME_BLUE'].width * scale, gfxUI['CHARACTER_NAME_BLUE'].height * scale);
     }
-    ctx.drawImage(gfxUI['UI_GFX_CHARACTER_BOXES'], 170 * scale, (myY + 54) * scale, gfxUI['UI_GFX_CHARACTER_BOXES'].width * scale, gfxUI['UI_GFX_CHARACTER_BOXES'].height * scale);
-    ctx.drawImage(gfxUI['UI_GFX_CHARACTER_BOX'], 168 * scale, (myY) * scale, gfxUI['UI_GFX_CHARACTER_BOX'].width * scale, gfxUI['UI_GFX_CHARACTER_BOX'].height * scale);
-    ctx.drawImage(gfxUI['UI_GFX_PORTRAITS'][ch.id], 176 * scale, (myY + 7) * scale, gfxUI['UI_GFX_PORTRAITS'][ch.id].width * scale, gfxUI['UI_GFX_PORTRAITS'][ch.id].height * scale);
+    ctx.drawImage(gfxUI['CHARACTER_BOXES'], 170 * scale, (myY + 54) * scale, gfxUI['CHARACTER_BOXES'].width * scale, gfxUI['CHARACTER_BOXES'].height * scale);
+    ctx.drawImage(gfxUI['CHARACTER_BOX'], 168 * scale, (myY) * scale, gfxUI['CHARACTER_BOX'].width * scale, gfxUI['CHARACTER_BOX'].height * scale);
+    ctx.drawImage(gfxUI['PORTRAITS'][ch.id], 176 * scale, (myY + 7) * scale, gfxUI['PORTRAITS'][ch.id].width * scale, gfxUI['PORTRAITS'][ch.id].height * scale);
 
 
     switch (championSelect[pl].mode) {
@@ -162,17 +162,17 @@ function showCharacterDetails(ch, pl, col, row) {
         case UI_CHARACTER_SELECT_SPELLBOOK:
             writeFontImage(ch.getName(), 170, (myY + 80), colourData['YELLOW']);
             drawSpellBook(player[pl]);
-            ctx.drawImage(gfxUI['UI_GFX_ICON_UNKNOWN'], 174 * scale, (myY + 56) * scale, gfxUI['UI_GFX_ICON_POCKETS'].width * scale, gfxUI['UI_GFX_ICON_POCKETS'].height * scale);
+            ctx.drawImage(gfxUI['ICON_UNKNOWN'], 174 * scale, (myY + 56) * scale, gfxUI['ICON_POCKETS'].width * scale, gfxUI['ICON_POCKETS'].height * scale);
             break;
         case UI_CHARACTER_SELECT_POCKET:
             drawPocketUI(player[pl], champion[ch.id], true);
             writeFontImage(ch.getName(), 170, (myY + 80), colourData['YELLOW']);
-            ctx.drawImage(gfxUI['UI_GFX_ICON_BOOKOFSKULLS'], 174 * scale, (myY + 56) * scale, gfxUI['UI_GFX_ICON_POCKETS'].width * scale, gfxUI['UI_GFX_ICON_POCKETS'].height * scale);
+            ctx.drawImage(gfxUI['ICON_BOOKOFSKULLS'], 174 * scale, (myY + 56) * scale, gfxUI['ICON_POCKETS'].width * scale, gfxUI['ICON_POCKETS'].height * scale);
             break;
         case UI_CHARACTER_SELECT_SCROLL:
             drawStatsPage(player[pl], champion[ch.id], true);
             writeFontImage(ch.getName(), 170, (myY + 80), colourData['YELLOW']);
-            ctx.drawImage(gfxUI['UI_GFX_ICON_POCKETS'], 174 * scale, (myY + 56) * scale, gfxUI['UI_GFX_ICON_POCKETS'].width * scale, gfxUI['UI_GFX_ICON_POCKETS'].height * scale);
+            ctx.drawImage(gfxUI['ICON_POCKETS'], 174 * scale, (myY + 56) * scale, gfxUI['ICON_POCKETS'].width * scale, gfxUI['ICON_POCKETS'].height * scale);
             break;
         case UI_CHARACTER_SELECT_START_GAME:
             var t = TEXT_PLAYER;
